d29375cbaf4d605a856a3c6e6d10e2bfe916a3d4
Thorsten Kramm <tk@system42.io>
2022-04-02 13:39:18 +0200

    use viper.unmarshal

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,34 +4,41 @@ import (
 	"fmt"
 	"github.com/cloudradar-monitoring/rport-pairing/deposit"
 	"github.com/spf13/viper"
+	"log"
 )
 
 type Config struct {
 	Server struct {
-		Address string
-		Url     string
-	}
-	StaticDeposit deposit.Deposit
-	DummyCode     string
+		Address string `mapstructure:"address"`
+		Url     string `mapstructure:"url"`
+	} `mapstructure:"server"`
+	StaticDeposit deposit.Deposit `mapstructure:"static-deposit"`
 }
 
 func New(confFile string) *Config {
 	viper.SetConfigName(confFile)
 	viper.SetConfigType("toml")
+	viper.AddConfigPath("/etc/rport/")
+	viper.AddConfigPath("$HOME/.rport")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+		panic(fmt.Errorf("Fatal error: %w \n", err))
 	}
-	config := Config{}
 	viper.SetDefault("server.address", "127.0.0.1:8080")
-	config.Server.Address = viper.GetString("server.address")
 	viper.SetDefault("server.url", "https://pairing.exmaple.com")
-	config.Server.Url = viper.GetString("server.url")
-	config.StaticDeposit.ConnectUrl = viper.GetString("static-deposit.connect_url")
-	config.StaticDeposit.Fingerprint = viper.GetString("static-deposit.fingerprint")
-	config.StaticDeposit.ClientId = viper.GetString("static-deposit.client_id")
-	config.StaticDeposit.Password = viper.GetString("static-deposit.password")
-	config.DummyCode = viper.GetString("static-deposit.code")
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+	//config := Config{}
+	//config.Server.Address = viper.GetString("server.address")
+	//config.Server.Url = viper.GetString("server.url")
+	//config.StaticDeposit.ConnectUrl = viper.GetString("static-deposit.connect_url")
+	//config.StaticDeposit.Fingerprint = viper.GetString("static-deposit.fingerprint")
+	//config.StaticDeposit.ClientId = viper.GetString("static-deposit.client_id")
+	//config.StaticDeposit.Password = viper.GetString("static-deposit.password")
+	//config.DummyCode = viper.GetString("static-deposit.code")
 	return &config
 }
