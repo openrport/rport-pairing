a9b470c73eca4601daf3ae64ee436ffce16d3636
Thorsten Kramm <tk@system42.io>
2022-04-06 16:35:01 +0200

    Run pairing service in background during test

--- .github/scripts/run_pairing_service.ps1
+++ .github/scripts/run_pairing_service.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
-
 # Start the pairing service
-go run cmd/rport-pairing.go  -c ./rport-pairing.conf.example >app.log
+[IO.File]::WriteAllLines("start_srv.bat", "go run cmd/rport-pairing.go  -c ./rport-pairing.conf.example")
+Start-Process -NoNewWindow start_srv.bat -RedirectStandardError start_srv.err.txt -RedirectStandardOutput start_srv.out.txt
 for ($i = 1; $i -le 10; $i++) {
     if ((Test-NetConnection -Port 9090 -ComputerName "127.0.0.1").TcpTestSucceeded) {
         break
