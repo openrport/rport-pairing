e8db06b778d7a08060a7500241c045d6c2d15549
Thorsten Kramm <tk@system42.io>
2022-04-07 21:55:19 +0200

    give service more time to start

--- .github/scripts/run_pairing_service.sh
+++ .github/scripts/run_pairing_service.sh
@@ -5,9 +5,16 @@ trap 'echo "exit $? due to $previous_command"' EXIT
 
 ## Start the pairing service
 go run cmd/rport-pairing.go  -c ./rport-pairing.conf.example >app.log 2>&1 &
-for C in $(seq 1 10);do
+for C in $(seq 1 30);do
   ncat -w1 -z 127.0.0.1 9090 && break
   echo "${C}: Waiting for server to come up"
-  sleep 1
+  sleep 3
 done
-pgrep -f "port-pairing -c ./rport-pairing.conf.example"
+if pgrep -f "port-pairing -c ./rport-pairing.conf.example";then
+  echo "pairing service is running"
+else
+  echo "Pairing service did not come up"
+  ps aux
+  test -e app.log && cat app.log
+  false
+fi
